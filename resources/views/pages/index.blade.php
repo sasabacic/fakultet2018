865774f623697b9d83dd3930b086eace08175998
sasabacic <salezd@yahoo.com>
2019-01-03 22:24:50 +0100

    vjezbe_laravel

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+
+@section('content')
+<h1>{{$title}}</h1>
+<p>This is the Laravel application from the 'Laravel from Scratch'</p>
+@endsection     
+
--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,39 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+//home ruta vraća blade view
+ Route::get('/', function () {
     return view('welcome');
+    });
+ 
+ 
+Route::middleware(['first', 'second'])->group(function() {
+//primjer closure funk
+Route::get('foo', function () {
+    return 'Hello';
+    })->name('profoo');
+//redirekcija vraća 404 not found
+Route::redirect('/here', '/there');
+ });
+//ruta vraća view sa parametrom
+Route::view('/studispis', 'studispis', ['name' => 'Marko']);
+
+//ruta sa adresnim get parametrom
+Route::get('user/{id}', function ($id) {
+    return 'User '.$id;
 });
 
-Route::get('foo', function () {
-    return 'Hello World';
+//ruta sa više adresnim get parametrom
+Route::get('posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Ovo je neki text<br>'.$postId.'<hr>komentar:<br>'.$commentId;
 });
+
+//Ovo je dinamička ruta
+Route::get('/users/{id}/{name}',function($id, $name){
+  return 'This is user '.$name.' with an id of '.$id;
+});
+
+
+Route::get('/', 'PagesController@index');
+Route::get('/about','PagesController@about');
+Route::get('/services','PagesController@services'); 
